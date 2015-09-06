a543f551324b52c4200f286bf30aec6e270f0207
Sergiu Dotenco <sergiu.dotenco@gmail.com>
2015-09-06 12:15:46 +0200

    fixed formatting

--- src/run_ORB_SLAM.cc
+++ src/run_ORB_SLAM.cc
@@ -1,23 +1,23 @@
 /**
-* This file is part of ORB-SLAM.
-*
-* Copyright (C) 2014 Raúl Mur-Artal <raulmur at unizar dot es> (University of Zaragoza)
-* Copyright (C) 2015 Sergiu Dotenco <sergiu.dotenco@fau.de> (University of Erlangen-Nürnberg)
-* For more information see <http://webdiis.unizar.es/~raulmur/orbslam/>
-*
-* ORB-SLAM is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* ORB-SLAM is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with ORB-SLAM. If not, see <http://www.gnu.org/licenses/>.
-*/
+ * This file is part of ORB-SLAM.
+ *
+ * Copyright (C) 2014 Raúl Mur-Artal <raulmur at unizar dot es> (University of Zaragoza)
+ * Copyright (C) 2015 Sergiu Dotenco <sergiu dot dotenco at fau dot de> (University of Erlangen-Nürnberg)
+ * For more information see <http://webdiis.unizar.es/~raulmur/orbslam/>
+ *
+ * ORB-SLAM is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ORB-SLAM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ORB-SLAM. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <iostream>
 
